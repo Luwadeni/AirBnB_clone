de7d190c31765c7e1fd64e1db5f82b9ad5d76fea
ifeoluwaoludamilare@gmail.com <ifeoluwaoludamilare@gmail.com>
2023-12-09 15:56:30 -0800

    user py

---
+++ models/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module (user): This file defines the UserModel sub class
+it inherits from BaseModel
+
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User models sub class inheriting from the main class BaseModel
+
+    Args:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
